0f35e63d751eed11b9c7642f5fa4ddba7eea9c6d
aksreenath123-code <aksreenath123@gmail.com>
2026-09-07 21:49:32 +0530

    Update ipo_agent.py

--- ipo_agent.py
+++ ipo_agent.py
@@ -28,8 +28,7 @@ from bs4 import BeautifulSoup
 from google import genai
 
 # ==================== 2. API കോൺഫിഗറേഷൻ ====================
-GEMINI_API_KEY1 = os.getenv("SREE_GEMINI_API_KEY1")
-GEMINI_API_KEY1 = os.getenv("SREE_GEMINI_API_KEY1")
+SREE_GEMINI_API_KEY1 = os.getenv("SREE_GEMINI_API_KEY1"
 SENDER_EMAIL = os.getenv("SENDER_EMAIL")
 GMAIL_APP_PASSWORD = os.getenv("GMAIL_APP_PASSWORD")
 RECEIVER_EMAIL = os.getenv("RECEIVER_EMAIL")
